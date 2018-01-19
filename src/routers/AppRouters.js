ea1b8c6657f371b7ea87438cdc01d115e9b3e86f
Dana Iti <dana.iti.work@gmail.com>
2018-01-19 15:33:01 +1300

    tidy up directory, removed base scss

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import App from '../components/YOUR_MAIN_PAGE';
+import App from '../components/App';
 
 const AppRouter = () => (
   <BrowserRouter>
