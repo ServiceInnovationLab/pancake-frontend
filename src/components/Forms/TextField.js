65ac415e2ce4f73cd97fa28d39142b60f8029e0b
dlouise64 <dana.iti.work@gmail.com>
2018-03-08 07:38:28 +1300

    added i18n

--- src/components/Forms/TextField.js
+++ src/components/Forms/TextField.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import '../../styles/TextField.css';
 
-const TextField = props => {
-  return (
-    <div className="input-group">
-      <label>
-        <span>{props.label}</span>
-        {props.isRequired && <span className="aria-hidden">(required)</span>}
-      </label>
-      {props.instructions && <p>{props.instructions}</p>}
-      <div>
-        <input type="text" {...props.input} />
-        {props.touched && props.error && <span>{props.error}</span>}
+class TextField extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleObj(obj){
+    if(Object.keys(obj).length > 1) {
+      return obj[this.props.lang];
+    } else {
+      return obj['en']
+    }
+  }
+
+  render(){
+    return (
+      <div className="input-group">
+        <label>
+          <span>{this.handleObj(this.props.label).label}</span>
+          {this.props.isRequired && <span className="aria-hidden">(required)</span>}
+        </label>
+        {this.props.instructions && <p>{this.handleObj(this.props.instructions).label}</p>}
+        <div>
+          <input type="text" {...this.props.input} />
+          {this.props.touched && this.props.error && <span>{this.props.error}</span>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default TextField;
