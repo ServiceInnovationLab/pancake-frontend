aa20d05217c0ac5b14b994c147a878d218a83fd3
Dana Iti <dana.iti.work@gmail.com>
2018-04-03 16:15:52 +1200

    WIP'

--- src/components/Forms/RenderCheckbox.js
+++ src/components/Forms/RenderCheckbox.js
@@ -1,22 +1,22 @@
 import React, { Fragment } from 'react';
 import { Field, FieldArray } from 'redux-form';
 import { camelCaser } from '../../helpers/strings';
+import '../../styles/Terms.css';
 
+export const RenderCheckbox = props => {
 
-const RenderCheckbox = props => {
-  
   let hasNestedValue = `has${camelCaser(props.label)}NestedValue`;
 
   return <Fragment>
-    {props.hasHeader && 
+    {props.hasHeader &&
       <fieldset className="checkbox-group">
-      <legend>Income</legend>
-      <p>Select any you receive</p>
-    </fieldset>}
-    <fieldset className="checkbox-group">
+        <legend>Income</legend>
+        <p>Select any you receive</p>
+      </fieldset>}
+    <fieldset className={props.theme === 'square' ? 'checkbox-square' : 'checkbox-group'}>
       <label>
         <input {...props.input} type="checkbox" />
-        {props.label && <span>{props.label}</span>}
+        {!props.hasPlainTextField ? <span>{props.label}</span> : <span>{props.text}</span>}
       </label>
       {props.hasValue && props.options && (
         <div className="radio-group">
@@ -25,23 +25,41 @@ const RenderCheckbox = props => {
             isRequired={true}
             options={props.options}
             component={RenderCheckboxRadio}
-            />
+          />
         </div>
       )}
-      
-      {props.hasValue && props.hasTextField && 
+
+      {props.hasValue && props.hasTextField &&
         <Field
           name={hasNestedValue}
           isRequired={true}
           component={RenderCheckboxTextField}
         />
       }
-      {props.hasValue && props.hasChildren &&
+      {props.hasValue && props.hasChildren && !props.hasPlainTextField &&
         <FieldArray name="otherIncome" component={renderOtherIncome} />
       }
+      {/* {console.log(props)} */}
+      {props.hasValue && props.hasChildren && props.hasPlainTextField && !props.hasExtraInfo &&
+        <ol className="terms">
+          <li>Every person commits an offence who
+          <ol>
+            <li>for the purpose of obtaining any rates rebate under this Act, for himself or for any other person, makes any statement or declaration knowing it to be false in any particular, or wilfully misleads or attempts to mislead any person concerned in the administration of this Act or any other person whatsoever; or
+            </li>
+            <li>refuses or fails to comply with any requirement under section 11, or refuses or fails to answer any question put to him pursuant to that section, or knowingly gives any false or misleading answer to any such question.</li>
+          </ol>
+          </li>
+          <li>Every person who commits an offence against this Act is liable on conviction before a District Court Judge to imprisonment for a term not
+exceeding 12 months or to a fine not exceeding $500, or to both.</li>
+        </ol>
+      }
+      {props.meta !== undefined && props.meta.touched && props.meta.error &&
+        <span className="error"><strong>Error: </strong>{props.meta.error}</span>
+      }
     </fieldset>
-  </Fragment>
+  </Fragment>;
 };
+
 
 const RenderCheckboxRadio = fields => {
   const {label, options, input} = fields;
@@ -61,7 +79,7 @@ const RenderCheckboxRadio = fields => {
     {fields.meta !== undefined && fields.meta.touched && fields.meta.error &&
       <span className="error"><strong>Error: </strong>{fields.meta.error}</span>
     }
-  </div>
+  </div>;
 };
 
 const RenderCheckboxTextField = fields => {
@@ -73,20 +91,20 @@ const RenderCheckboxTextField = fields => {
         type="number"
         style={{width: '100%', margin: '0'}}
       />
-        
+
       {fields.meta !== undefined && fields.meta.touched && fields.meta.error &&
         <span className="error"><strong>Error: </strong>{fields.meta.error}</span>
       }
     </div>
-  </div>
+  </div>;
 };
 
 const renderOtherIncome = ({ fields, meta: { error, submitFailed, valid } }) => (
-  
+
   <ul className="nested-list">
     {fields.map((income, index) => (
       <li key={index} style={{marginBottom: '15px'}}>
-        
+
         <h4>Other income #{index + 1} </h4>
         <button
           type="button"
