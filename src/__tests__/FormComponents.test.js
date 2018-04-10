4713d6921aef3cd40762cb1b7813682b141a34f3
dlouise64 <dana.iti.work@gmail.com>
2018-04-10 14:33:09 +1200

    remove unused component

--- src/__tests__/FormComponents.test.js
+++ src/__tests__/FormComponents.test.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import RenderCheckbox from '../components/Forms/RenderCheckbox';
 import RenderTextField from '../components/Forms/RenderTextField';
 import RenderRadio from '../components/Forms/RenderRadio';
 
 describe('components', function() {
 
-  describe('<RenderCheckbox />', function() {
-    it('renders correctly', function() {
-      let tree = renderer.create(<RenderCheckbox />).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
-  });
+
 
   describe('<RenderRadio />', function() {
     it('renders correctly', function() {
