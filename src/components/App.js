39d39ade07d2b035c73bb7964a85a73cce00016d
dlouise64 <dana.iti.work@gmail.com>
2018-04-10 14:06:37 +1200

    Reduce lines in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,35 +13,31 @@ export class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="container">
-          <section>
-            <h1>The Rates Rebate Scheme provides a discount to low-income<br/> homeowners on the cost of their rates.</h1>
-            <p>If you are a low-income homeowner you could get a discount or partial refund of up to $620 on your property rates.</p>
+      <div className="container">
+        <section>
+          <h1>The Rates Rebate Scheme provides a discount to low-income<br/> homeowners on the cost of their rates.</h1>
+          <p>If you are a low-income homeowner you could get a discount or partial refund of up to $620 on your property rates.</p>
 
-            <div>
-              <Link className="btn btn-primary" to={'apply'}>Apply now</Link>
-            </div>
-          </section>
+          <Link className="btn btn-primary" to={'apply'}>Apply now</Link>
+        </section>
 
-          <section>
-            <h3>What is a Rates Rebate?</h3>
-            <p>
-            Rates rebates are a subsidy that gives you a discount on the rates bill of your residential property.<br />
-              <a className="external-link"><span>Learn more about Rates rebates.</span></a></p>
-          </section>
+        <section>
+          <h3>What is a Rates Rebate?</h3>
+          <p>
+          Rates rebates are a subsidy that gives you a discount on the rates bill of your residential property.<br />
+            <a className="external-link"><span>Learn more about Rates rebates.</span></a></p>
+        </section>
 
-          <section>
-            <h3>Who can get a rates rebates?</h3>
-            <p>Anyone can receive a rebate for the property they live in, as long as they met the criteria.</p>
-            <ul>
-              <li>You must live in the property for the rebate you are applying for. You can only get a rebate for one property that you own.</li>
-              <li>You must be the title holder</li>
-              <li>Have income?</li>
-            </ul>
-            <a className="external-link"><span>Learn more about Rates rebates.</span></a>
-          </section>
-        </div>
+        <section>
+          <h3>Who can get a rates rebates?</h3>
+          <p>Anyone can receive a rebate for the property they live in, as long as they met the criteria.</p>
+          <ul>
+            <li>You must live in the property for the rebate you are applying for. You can only get a rebate for one property that you own.</li>
+            <li>You must be the title holder</li>
+            <li>Have income?</li>
+          </ul>
+          <a className="external-link"><span>Learn more about Rates rebates.</span></a>
+        </section>
       </div>
     );
   }
