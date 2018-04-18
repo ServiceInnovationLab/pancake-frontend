cda32927c62354a266e6ead26d48a7f8a112e0ea
dlouise64 <dana.iti.work@gmail.com>
2018-04-19 09:16:15 +1200

    Post form data to API. Code formatting. Removed HashRouter

--- src/components/FirstTimeApplicant/Landing.js
+++ src/components/FirstTimeApplicant/Landing.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import Page1 from './Page1';
-// import Page2 from './Page2';
 
 class Landing extends React.Component {
   constructor(props) {
     super(props);
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
+    this.nextPage = this
+      .nextPage
+      .bind(this);
+    this.previousPage = this
+      .previousPage
+      .bind(this);
     this.state = {
       page: 1
     };
   }
   nextPage() {
-    this.setState({ page: this.state.page + 1 });
+    this.setState({
+      page: this.state.page + 1
+    });
   }
 
   previousPage() {
-    this.setState({ page: this.state.page - 1 });
+    this.setState({
+      page: this.state.page - 1
+    });
   }
 
   render() {
-    const { onSubmit } = this.props;
-    const { page } = this.state;
+    const {page} = this.state;
     return (
       <div>
-        {/* {page === 1 && <Page1 onSubmit={this.nextPage} />} */}
-        {page === 1 && (
-          <Page1
-            // previousPage={this.previousPage}
-            onSubmit={onSubmit}
-          />
-        )}
+        {page === 1 && (<Page1/>)}
       </div>
     );
   }
