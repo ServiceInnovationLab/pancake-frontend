5fe54156e1c0955572f284e87315e1fca1ad6d00
dlouise64 <dana.iti.work@gmail.com>
2018-04-19 11:55:10 +1200

    fix origin url to server, fix image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer id="footer">
       <div className="container">
-        <img src="https://serviceinnovationlab.github.io/FamilyServiceDirectorySearchinator/footer-logo-govt.png" alt="New Zealand Government"/>
+        <img src="footer-logo-govt.png" srcSet="footer-logo-govt@2x.png 2x,footer-logo-govt.png 1x" width="240" height="46" alt="New Zealand Government" />
       </div>
     </footer>
   );
