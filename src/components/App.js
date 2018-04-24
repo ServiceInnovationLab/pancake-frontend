85d2e027061b92dd82bf8f08ebc87ea9db26ab8b
dlouise64 <dana.iti.work@gmail.com>
2018-04-24 13:11:30 +1200

    Tidyup

--- src/components/App.js
+++ src/components/App.js
@@ -13,60 +13,8 @@ export class App extends Component {
 
   render() {
     return (
-      // <div className="container">
-      //   <section>
-      //     <h1>The Rates Rebate Scheme provides a discount to low-income<br/> homeowners on the cost of their rates.</h1>
-      //     <p>If you are a low-income homeowner you could get a discount or partial refund of up to $620 on your property rates.</p>
-
-      //     <Link className="btn btn-primary" to='/apply'>Apply now</Link>
-      //   </section>
-
-      //   <section>
-      //     <h3>What is a Rates Rebate?</h3>
-      //     <p>
-      //     Rates rebates are a subsidy that gives you a discount on the rates bill of your residential property.<br />
-      //       <a className="external-link"><span>Learn more about Rates rebates.</span></a></p>
-      //   </section>
-
-      //   <section>
-      //     <h3>Who can get a rates rebates?</h3>
-      //     <p>Anyone can receive a rebate for the property they live in, as long as they met the criteria.</p>
-      //     <ul>
-      //       <li>You must live in the property for the rebate you are applying for. You can only get a rebate for one property that you own.</li>
-      //       <li>You must be the title holder</li>
-      //       <li>Have income?</li>
-      //     </ul>
-      //     <a className="external-link"><span>Learn more about Rates rebates.</span></a>
-      //   </section>
-      // </div>
       <Fragment>
         <div></div>
-        {/* <div className="container autocomplete-form">
-          <h1 className="heading-primary">If you are a low-income homeowner you could get a discount or partial refund of up to $620 on your property rates with a rates rebate.</h1>
-          <hr/>
-          <h2 className="heading-secondary">Find out if you could get a rebate</h2>
-          <p>Enter your details here to check the eligibility before you apply</p>
-
-          <div className="primary-quote">
-            I live at <span><input type="text" value="123 Example Street, Tauranga."/></span><br/>
-            My rates are <span>$<input type="text" value="0" className="input-integer" /></span><br/>
-            I earn <span>$<input type="text" value="0" /></span> a year,<br/>
-            and have <span><input type="text" value="0" className="input-integer" /></span> dependants.
-          </div>
-
-          <div>
-            <h2>You are eligible for $620</h2>
-            <p>Assuming you meet the criteria</p>
-          </div>
-
-          <div>
-            <h2>What is a Rates Rebate?</h2>
-            <p>Rates rebates are a subsidy that gives you a discount on the rates bill of your residential property.</p>
-            <p>Any homeowner may receive a rebate for the property they live in, as long as they meet the criteria. This is calculated by your property rates, your income for the last tax year, and the number of dependants you have. If you have dependants, the upper threshold of your income can be $500 more for each dependant in your care. For example, if you have 2 children, the top limit of how much you could earn to be entitled to the full rebate would be $1000 more than someone with no dependants.</p>
-          </div>
-
-          <button className="btn-primary">Apply now</button>
-        </div> */}
       </Fragment>
     );
   }
--- src/components/FirstTimeApplicant/WizardFormSecondPage.js
+++ src/components/FirstTimeApplicant/WizardFormSecondPage.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import {Link} from 'react-router-dom';
 import { Field, reduxForm, formValueSelector} from 'redux-form';
 import {connect} from 'react-redux';
 import validate from '../../helpers/validate'
@@ -75,7 +76,9 @@ import config from '../../config';
       //   .then(res => window.location.href = '#/page2')
       //   .catch(err => console.log('Error occurred: Check origin has been enabled correctly on the server', err));
     }
-
+    goHome() {
+      return window.location = '#/';
+    }
     render() {
       const {
         handleSubmit,
@@ -84,30 +87,12 @@ import config from '../../config';
       return (
         <Fragment>
           <div className="container">
-            <a className="previous" onClick={this.previousPage} style={{'color': '#aaa', 'marginTop': '15px','marginBottom': '60px', 'display': 'inline-block'}}>
+          {/* {console.log('pr', this.previousPage())} */}
+            <a onClick={()=>{window.location.reload()}} style={{'color': '#aaa', 'marginTop': '15px','marginBottom': '60px', 'display': 'inline-block'}}>
             &larr; Home
             </a>
-            <h2 className="heading-secondary green">What you will need to do to apply for a rebate</h2>
             
-            <section>
-              <h3 className="heading-secondary grey">Step 1</h3>
-              <p>You will need to know your total income for the 2016/2017 Tax year (1 March 2016 - 31 March 2017). This includes rental income from any properties you own, interest and dividends, and overseas income (converted to $NZD).</p>
-              <p>You can get this from a few places, such as Inland Revenue, by calling them on 0800 775 247 or logging on to your MyIR account at IRD.govt.nz, from Ministry of Social Development, through your employer, accountant etc. </p>
-            </section>
-
-            <section>
-              <h3 className="heading-secondary grey">Step 2</h3>
-              <p>Fill out this form and submit it</p>
-            </section>
-
-            <section>
-              <h3 className="heading-secondary grey">Step 3</h3>
-              <p>Make a declaration in front of an authorised witness (you can do this by visiting your local council). All you need to bring with you is your proof of income.</p>
-            </section>
-
-            <hr />
-            <h2 className="heading-secondary green">Step 2: Apply for a rates rebate</h2>
-            <h3 className="heading-secondary grey">This is for the 1 July 2017 - 30 June 2018 rating year</h3>
+            <Head/>
             <form onSubmit={handleSubmit(this.saveFormData)} className="container form-inner">
               {firstTimeApplication.map((field, key) => {
                 let label = field.label['en'].text;
@@ -130,36 +115,80 @@ import config from '../../config';
                   placeholder={field.placeholder && field.placeholder['en'].text}
                   hasAddressFinder={field.hasAddressFinder}/>);
               })}
-              
-              <h2>We have calculated that your entitlement is $620</h2>
-              <p>This will be applied to your rates account once your application has been fully proccessed.</p>
-              <div>
-              <button type="submit" className="next btn-primary">
-                Submit Application
-              </button>
-            </div>
-            {this.state.complete && 
-            <div>
-            <h2 className="heading-secondary">Step 3: Get your application witnessed</h2>
-
-            <h3>You are almost there!</h3>
-
-            <h4>Your application form has been digitally sent to your local council - now you have to have your signed declaration witnessed for your rebate to be processed.</h4>
-
-            <p>To do this you can visit one of your local council’s service centres during opening hours and let the staff at the service desk know you are there to sign your rates rebates application.</p>
-
-            <p>The only thing you need to bring with you is your proof of income.</p>
-
-            <p>A copy of your answers to the application has been sent to your email, so if you can't make it to the service centre, you can print out a copy of your application and get your declaration witnessed and signed by an authorised witness such as a JP or Minister for Religion.</p>
-            
-            <a className="btn btn-primary">Find my nearest service centre</a>
-              </div>}
+              <Calculated/>
+              <Submit/>
+              {this.state.complete && <Foot/>}
             </form>
           </div>
         </Fragment>
       )
     }
   }
+
+const Head = () => {
+  return (
+    <Fragment>
+      <h2 className="heading-secondary green">What you will need to do to apply for a rebate</h2>
+      <section>
+        <h3 className="heading-secondary grey">Step 1</h3>
+        <p>You will need to know your total income for the 2016/2017 Tax year (1 March 2016 - 31 March 2017). This includes rental income from any properties you own, interest and dividends, and overseas income (converted to $NZD).</p>
+        <p>You can get this from a few places, such as Inland Revenue, by calling them on 0800 775 247 or logging on to your MyIR account at IRD.govt.nz, from Ministry of Social Development, through your employer, accountant etc. </p>
+      </section>
+
+      <section>
+        <h3 className="heading-secondary grey">Step 2</h3>
+        <p>Fill out this form and submit it</p>
+      </section>
+
+      <section>
+        <h3 className="heading-secondary grey">Step 3</h3>
+        <p>Make a declaration in front of an authorised witness (you can do this by visiting your local council). All you need to bring with you is your proof of income.</p>
+      </section>
+
+      <hr />
+      <h2 className="heading-secondary green">Step 2: Apply for a rates rebate</h2>
+      <h3 className="heading-secondary grey">This is for the 1 July 2017 - 30 June 2018 rating year</h3>
+    </Fragment>
+  );
+}
+
+const Foot = () => {
+  return (
+    <div>
+      <h2 className="heading-secondary">Step 3: Get your application witnessed</h2>
+
+      <h3>You are almost there!</h3>
+
+      <h4>Your application form has been digitally sent to your local council - now you have to have your signed declaration witnessed for your rebate to be processed.</h4>
+
+      <p>To do this you can visit one of your local council’s service centres during opening hours and let the staff at the service desk know you are there to sign your rates rebates application.</p>
+
+      <p>The only thing you need to bring with you is your proof of income.</p>
+
+      <p>A copy of your answers to the application has been sent to your email, so if you can't make it to the service centre, you can print out a copy of your application and get your declaration witnessed and signed by an authorised witness such as a JP or Minister for Religion.</p>
+      
+      <a className="btn btn-primary">Find my nearest service centre</a>
+    </div>
+  );
+}
+
+const Calculated = () => {
+  return (
+    <Fragment>
+      <h2>We have calculated that your entitlement is $620</h2>
+      <p>This will be applied to your rates account once your application has been fully proccessed.</p>
+    </Fragment>
+  );
+}
+const Submit = () => {
+  return (
+    <div>
+      <button type="submit" className="next btn-primary">
+        Submit Application
+      </button>
+    </div>
+  );
+}
 
 WizardFormSecondPage = reduxForm({
   form: 'wizard',
