69aac8cbf5f16da31f4d58bee7ce4789a7816e86
Dana Iti <dana.iti.work@gmail.com>
2018-05-07 16:04:50 +1200

    Add benefit schedule

---
+++ src/JSONFormData/BenefitSchedule.js
@@ -0,0 +1,118 @@
+const BenefitSchedule = [
+  {
+    'BenefitCatagory': 'NZ Superannuation',
+    'Situation': 'Single living alone',
+    'YearEnded2017': '23,058.36',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'NZ Superannuation',
+    'Situation': 'Single sharing',
+    'YearEnded2017': '21,191.56',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'NZ Superannuation',
+    'Situation': 'Married /civil union /de facto couple (each)',
+    'YearEnded2017': '17,458.48',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'NZ Superannuation',
+    'Situation': 'Married /civil union /de facto couple',
+    'YearEnded2017': '34,916.96',
+    'Children': ''
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Single 18-19 years (away from home)',
+    'YearEnded2017': '10,173.28',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Single 20-24 years',
+    'YearEnded2017': '10,173.28',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Single 25 and over',
+    'YearEnded2017': '12,208.56',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Married /civil union /de facto couple',
+    'YearEnded2017': '20,346.56',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Married /civil union /de facto couple (each)',
+    'YearEnded2017': '10,173.28',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Sole parent',
+    'YearEnded2017': '19,358.56',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Married /civil union /de facto couple',
+    'YearEnded2017': '21,799.44',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Jobseeker Support',
+    'Situation': 'Married /civil union /de facto couple (each)',
+    'YearEnded2017': '10,899.72',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Sole Parent Support',
+    'Situation': 'Sole parent',
+    'YearEnded2017': '19,358.56',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Single 18 and over',
+    'YearEnded2017': '15,366.52',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Married /civil union /de facto couple (each)',
+    'YearEnded2017': '12,716.08',
+    'Children': 'Without'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Sole parent',
+    'YearEnded2017': '22,134.84',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Married /civil union /de facto couple 1 child',
+    'YearEnded2017': '26,884.00',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Married /civil union /de facto couple with 2 or more children',
+    'YearEnded2017': '26,884.00',
+    'Children': 'With'
+  },
+  {
+    'BenefitCatagory': 'Supported Living Payment',
+    'Situation': 'Married /civil union /de facto couple (each)',
+    'YearEnded2017': '13,442.00',
+    'Children': 'With'
+  }
+];
+
+export default BenefitSchedule;
