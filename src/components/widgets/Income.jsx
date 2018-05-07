8c96186c66480bf89b8561a8680078b24d1595a7
Awesome Code <team@xinminlabs.com>
2018-05-07 19:56:48 +0000

    Auto corrected by following react/jsx-curly-brace-presence

--- src/components/widgets/Income.jsx
+++ src/components/widgets/Income.jsx
@@ -109,7 +109,7 @@ class Income extends React.Component {
             component={RenderRadio}
             options={earnLessThan.options && earnLessThan.options['en']}
             onChange={this.handleSelection}
-            className={'radio-group-income'}
+            className="radio-group-income"
           />
         </section>
       );
