b23e30caa72edc73d9d33d6251f505777339b81b
Brenda Wallace <shiny@cpan.org>
2018-05-07 17:55:40 +1200

    Manual entry of income supported

--- src/components/widgets/Income.jsx
+++ src/components/widgets/Income.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import config from '../../config';
 import RenderRadio from '../../components/Forms/RenderRadio';
+import renderField from '../Forms/renderField';
 import {Field} from 'redux-form';
 
 class Income extends React.Component {
@@ -9,18 +10,24 @@ class Income extends React.Component {
     super(props);
     this.state = {show_input: false};
     this.handleSelection = this.handleSelection.bind(this);
+    this.handleManualIncome = this.handleManualIncome.bind(this);
+  }
+
+  handleManualIncome(event, newValue, previousValue, name) {
+    this.setState({income: newValue});
+    this.props.onSelection(newValue, 'exact');
   }
 
   handleSelection(event, newValue, previousValue, name) {
     let income;
-    if (newValue === 'between') {
-      this.setState({show_input: true});
-    } else if (newValue === 'below') {
+    if (newValue === 'below') {
       income = this.state.maximum_income_for_full_rebate;
+      this.setState({show_input: false});
     } else if (newValue === 'above') {
       income = this.state.minimum_income_for_no_rebate;
+
     }
-    this.setState({income: income});
+    this.setState({income: income, show_input: (newValue == 'between')});
     this.props.onSelection(income, newValue);
   }
 
@@ -99,9 +106,11 @@ class Income extends React.Component {
     if (this.state.minimum_income_for_no_rebate) {
       let earnLessThan = this.getOptions();
       return (
+            <div className="arrow-box primary">
+              <div>
         <section>
           <label htmlFor="earn_less_than">
-            I earn
+            From 1st April 2016 to 31 March 2017 I earned
           </label>
 
           <Field
@@ -111,7 +120,20 @@ class Income extends React.Component {
             onChange={this.handleSelection}
             className={'radio-group-income'}
             />
+
+            {this.state.show_input &&
+            <div>
+              <label>My annual income was</label>
+              <Field
+                name="income"
+                onChange={this.handleManualIncome}
+                type="text"
+                component={renderField}/>
+              <span class="reassurance">Approximate values are fine. You'll need the real values if you choose to apply</span>
+            </div>
+            }
         </section>
+        </div></div>
       );
     }
     else {
