51ed80ac0af58c3a650d1385f981cf01ab9157dd
Brenda Wallace <shiny@cpan.org>
2018-05-09 11:29:23 +1200

    More small grammar fix ups, and line wrapp

--- src/JSONFormData/FirstTimeApplication.js
+++ src/JSONFormData/FirstTimeApplication.js
@@ -92,10 +92,24 @@ var firstTimeApplication = [
     },
     'accordianText': {
       'en': {
-        'text': 'If your name isn’t on the RID because you are a retirement village resident and/or have a License to Occupy you will need to fill in an additional declaration form and bring it with you when visit the council. This can be found here <a href="https://www.dia.govt.nz/Pubforms.nsf/URL/OwnerOccupierDeclarationFormJuly2011.pdf/$file/OwnerOccupierDeclarationFormJuly2011.pdf">here</a> <br/><br/> <strong>Please note: Retirement Villages that became elegible to apply for a rates rebate under the Rates Rebate (Retirement Village Residents) Amendment Act 2018 cannot apply for a rebate until 1 July 2018.</strong>'
+        'text': 'If your name isn’t on the RID because you are a retirement village \
+                resident and/or have a License to Occupy you will need to fill in \
+                <a href="https://www.dia.govt.nz/Pubforms.nsf/URL/OwnerOccupierDeclarationFormJuly2011.pdf/$file/OwnerOccupierDeclarationFormJuly2011.pdf">\
+                an additional declaration form</a>\
+                and bring it with you when visit the council. <br/><br/>\
+                <strong>Please note: Retirement Villages that became eligible to\
+                apply for a rates rebate under the Rates Rebate (Retirement Village Residents)\
+                Amendment Act 2018 cannot apply for a rebate until 1 July 2018.</strong>'
       },
       'mi': {
-        'text': 'If your name isn’t on the RID because you are a retirement village resident or an owner-occupiers you will need to fill in an additional declaration form.'
+        'text': 'If your name isn’t on the RID because you are a retirement village \
+                resident and/or have a License to Occupy you will need to fill in \
+                <a href="https://www.dia.govt.nz/Pubforms.nsf/URL/OwnerOccupierDeclarationFormJuly2011.pdf/$file/OwnerOccupierDeclarationFormJuly2011.pdf">\
+                an additional declaration form</a>\
+                and bring it with you when visit the council. <br/><br/>\
+                <strong>Please note: Retirement Villages that became eligible to\
+                apply for a rates rebate under the Rates Rebate (Retirement Village Residents)\
+                Amendment Act 2018 cannot apply for a rebate until 1 July 2018.</strong>'
       }
     }
   },
