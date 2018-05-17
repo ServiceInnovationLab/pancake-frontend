2ce041529f98e0c78445ef160eb9ca15a404c9b6
Brenda Wallace <shiny@cpan.org>
2018-05-17 14:05:52 +1200

    Fix error message display on fields

--- src/components/Forms/Error.js
+++ src/components/Forms/Error.js
@@ -3,13 +3,13 @@ import React, {Fragment} from 'react';
 const Error = props => {
   const {fields} = props;
   return <Fragment>
-    {fields.length > 0 && <div>
+    <div>
       {fields && fields.touched && fields.error &&
         <div>
           <span className="error"><strong>Error: </strong>{fields.error}</span>
         </div>
       }
-    </div>}
+    </div>
   </Fragment>;
 };
 
