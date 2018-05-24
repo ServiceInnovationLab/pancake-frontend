230dc860ff5738a0110b6e3d44a80950610a6094
Awesome Code <team@xinminlabs.com>
2018-05-24 09:26:18 +0000

    Auto corrected by following react/jsx-closing-tag-location

--- src/components/widgets/Rebate.js
+++ src/components/widgets/Rebate.js
@@ -39,7 +39,7 @@ class Rebate extends React.Component {
       axios
         .post(`${config.OPENFISCA_ORIGIN}`, data)
         .then(res => {
-          let rebate = res.data.properties.property_1.rates_rebate['2018']
+          let rebate = res.data.properties.property_1.rates_rebate['2018'];
           this.setState({rebate: rebate});
         })
         .catch(err => console.log('err fetching properties', err));
