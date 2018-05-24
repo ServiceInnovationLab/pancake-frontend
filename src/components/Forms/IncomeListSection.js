82d4776252934cd5fa089891d92b6af2f9607a80
Awesome Code <team@xinminlabs.com>
2018-05-24 09:56:20 +0000

    Auto corrected by following react/jsx-indent

--- src/components/Forms/IncomeListSection.js
+++ src/components/Forms/IncomeListSection.js
@@ -69,9 +69,9 @@ class IncomeListSection extends React.Component {
           <fieldset>
             <label
               style={{
-              fontSize: '20px',
-              fontWeight: '500'
-            }}>What was your total income for the 2017/18 tax year?</label>
+                fontSize: '20px',
+                fontWeight: '500'
+              }}>What was your total income for the 2017/18 tax year?</label>
             <p>You will need to know your total income for the 2016/2017 Tax year (1 March
               2016 - 31 March 2017) including rental income from any properties you own,
               interest and dividends, and overseas income (converted to $NZD).
