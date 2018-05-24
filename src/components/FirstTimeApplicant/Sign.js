79a3452ac8cfc215e64fcccf67af9f5cbb0f835b
Brenda Wallace <shiny@cpan.org>
2018-05-24 12:25:39 +1200

    Hide the signature fields after submitting

--- src/components/FirstTimeApplicant/Sign.js
+++ src/components/FirstTimeApplicant/Sign.js
@@ -14,18 +14,18 @@ class Sign extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      applicant_role: '',
+      complete: false,
+      error: false,
+      fields: {},
       lng: 'en',
       page: 2,
       shown: false,
-      complete: false,
+      sig1: '',
       signature: '',
       stage: '',
-      fields: {},
       witness_name: '',
-      applicant_role: '',
-      witness_role: '',
-      sig1: '',
-      error: false
+      witness_role: ''
     };
     this.nextPage = this
       .nextPage
@@ -114,55 +114,65 @@ class Sign extends React.Component {
           <p>The combined income of myself and joint-owners and partners living with me on 1 July 2017 was $<strong>{this.state.fields.income}</strong></p>
 
           {this.state.rebate &&
-            <p>Your claimed rebate is ${this.state.rebate}</p>
+            <Fragment>
+            <h3>Rebate Claimed</h3>
+            <p>Your claimed rebate is <strong className="rebate-amount">${this.state.rebate}</strong></p>
+            </Fragment>
           }
 
-          <h2>Sign here</h2>
+          {!this.state.complete &&
+            <Fragment>
+              <h2>Sign here</h2>
 
-          <form onSubmit={handleSubmit(this.submitSignature)} className="container form-inner">
-            <Accordian
-              label="It is an offence to knowingly make a false statement in your application"
-              text="Section 14 of the Rates Rebate Act 1973<br/>14. Offences <ul><li>(1) Every person commits an offence who
-              for the purpose of obtaining any rates rebate under this Act, for himself or for any other person, makes any statement or declaration knowing it to be false in any particular, or wilfully misleads or attempts to mislead any person concerned in the administration of this Act or any other person whatsoever; or </li><li>(b) refuses or fails to comply with any requirement under section 11, or refuses or fails to answer any question put to him pursuant to that section, or knowingly gives any false or misleading answer to any such question.</li><li>(2) Every person who commits an offence against this Act is liable on conviction before a District Court Judge to imprisonment for a term not
-              exceeding 12 months or to a fine not exceeding $500, or to both.</li></ul>"
-            />
-            <h3 style={{marginTop: '80px'}}>Applicant</h3>
-            <p>I <b>{this.state.fields.full_name}</b> of <b>{this.state.fields.address}</b>, solemnly and sincerely
-              declare that I believe the information I have given on this form is true and
-              correct, and I make this solemn declaration conscientiously believing the same
-              to be true and by virtue of the Oaths and Declarations Act 1957.
-            </p>
-            <SignatureCanvas
-              onEnd={()=>this.setState({sig1: this.sigCanvas.toDataURL()})}
-              penColor='black' ref={(ref) => { this.sigCanvas = ref }} canvasProps={{width: 500, height: 300, className: 'sigCanvas'}} />
-            <button onClick={()=>{this.sigCanvas.clear()}}>Clear</button>
+              <form onSubmit={handleSubmit(this.submitSignature)} className="container form-inner">
+                <Accordian
+                  label="It is an offence to knowingly make a false statement in your application"
+                  text="Section 14 of the Rates Rebate Act 1973<br/>14. Offences <ul><li>(1) Every person commits an offence who
+                  for the purpose of obtaining any rates rebate under this Act, for himself or for any other person, makes any statement or declaration knowing it to be false in any particular, or wilfully misleads or attempts to mislead any person concerned in the administration of this Act or any other person whatsoever; or </li><li>(b) refuses or fails to comply with any requirement under section 11, or refuses or fails to answer any question put to him pursuant to that section, or knowingly gives any false or misleading answer to any such question.</li><li>(2) Every person who commits an offence against this Act is liable on conviction before a District Court Judge to imprisonment for a term not
+                  exceeding 12 months or to a fine not exceeding $500, or to both.</li></ul>"
+                />
+                <h3 style={{marginTop: '80px'}}>Applicant</h3>
+                <p>I <b>{this.state.fields.full_name}</b> of <b>{this.state.fields.address}</b>, solemnly and sincerely
+                  declare that I believe the information I have given on this form is true and
+                  correct, and I make this solemn declaration conscientiously believing the same
+                  to be true and by virtue of the Oaths and Declarations Act 1957.
+                </p>
+                <SignatureCanvas
+                  onEnd={()=>this.setState({sig1: this.sigCanvas.toDataURL()})}
+                  penColor='black' ref={(ref) => { this.sigCanvas = ref }} canvasProps={{width: 500, height: 300, className: 'sigCanvas'}} />
+                <div><button onClick={()=>{this.sigCanvas.clear()}}>Clear</button></div>
 
-            <h3>Witness</h3>
-            <p>Declared at {new Date().toLocaleString()} before me</p>
-            <div style={{margin: '30px 0'}}>
-              <label>Witness Name</label>
-              <WitnessField
-                onChange={e=>this.setState({witness_name: e.target.value})}
-                name="witness_name"
-              />
-              <label>Role</label>
-              <WitnessField
-                onChange={e=>this.setState({witness_role: e.target.value})}
-                name="witness_role"
-              />
-            </div>
-            <SignatureCanvas
-              onEnd={()=>this.setState({sig2: this.sigCanvas2.toDataURL()})}
-              penColor='black' ref={(ref) => { this.sigCanvas2 = ref }} canvasProps={{width: 500, height: 300, className: 'sigCanvas'}} />
-            <button onClick={()=>{this.sigCanvas2.clear()}}>Clear</button>
-            <Submit/> {this.state.complete && <Foot/>}
-          </form>
+                <h3>Witness</h3>
+                <p>Declared at {new Date().toLocaleString()} before me</p>
+                <div style={{margin: '30px 0'}}>
+                  <label>Witness Name</label>
+                  <WitnessField
+                    onChange={e=>this.setState({witness_name: e.target.value})}
+                    name="witness_name"
+                  />
+                  <label>Role</label>
+                  <WitnessField
+                    onChange={e=>this.setState({witness_role: e.target.value})}
+                    name="witness_role"
+                  />
+                </div>
+                <SignatureCanvas
+                  onEnd={()=>this.setState({sig2: this.sigCanvas2.toDataURL()})}
+                  penColor='black' ref={(ref) => { this.sigCanvas2 = ref }} canvasProps={{width: 500, height: 300, className: 'sigCanvas'}} />
+                <div><button onClick={()=>{this.sigCanvas2.clear()}}>Clear</button></div>
+                <Submit/>
+                </form>
+              </Fragment>
+            }
+            {this.state.complete && <Foot/>}
         </div>}
         {this.state.error && <div className="container"><h2>Oops, this application does not exist.</h2></div>}
       </Fragment>
     )
   }
 }
+
+
 
 const WitnessField = props => {
   return <input type="text" name={props.name} onChange={props.onChange} />
