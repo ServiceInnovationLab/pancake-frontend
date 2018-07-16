5226d6540364281cbd6dffaa7332d7574ef93b9d
Awesome Code <team@xinminlabs.com>
2018-07-16 05:51:39 +0000

    Auto corrected by following Javascript no-trailing-spaces

--- src/components/Forms/TextField.js
+++ src/components/Forms/TextField.js
@@ -39,7 +39,7 @@ export default class TextField extends React.Component {
         <legend>{this.props.label}</legend>
         {this.props.type === 'number' ? <NumberField {...this.props} value={showValue} /> : <input type="text" {...this.props.input} value={showValue} />}
         {this.props.instructions && <p className="instructions" dangerouslySetInnerHTML={{ __html: this.props.instructions }}></p>}
-        {this.props.checkboxFieldName && 
+        {this.props.checkboxFieldName &&
           <Field component={Checkbox} label={this.props.checkboxLabel} name={this.props.checkboxFieldName} />}
         {this.props.accordianText && <div><Accordian label={this.props.accordianLabel} text={this.props.accordianText} /></div>}
         <ErrorMessage fields={this.props.meta} />
