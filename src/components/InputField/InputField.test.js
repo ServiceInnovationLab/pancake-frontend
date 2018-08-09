691e09f2694a0d0a0cbd6a606e6f2524ccacc745
Dana Iti <dana.iti.work@gmail.com>
2018-08-10 01:45:44 +1200

    input field snapshot and more tests

--- src/components/InputField/InputField.test.js
+++ src/components/InputField/InputField.test.js
@@ -7,4 +7,12 @@ describe('<InputField />', () => {
     const input = shallow(<InputField />);
     expect(input.find('input').length).toEqual(1);
   });
+
+  it('toggles', () => {
+    const mockFn = jest.fn();
+    const input = shallow(<InputField inputHandler={mockFn}/>);
+    input.simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+    expect(input).toMatchSnapshot();
+  });
 });
--- src/components/RadioGroup/RadioGroup.test.js
+++ src/components/RadioGroup/RadioGroup.test.js
@@ -11,4 +11,16 @@ describe('<RadioGroup />', () => {
     );
     expect(input.find('InputField').length).toEqual(3);
   });
+  it('toggles a radio group', () => {
+    const input = shallow(
+      <RadioGroup
+        name="question_1"
+        radios={['yes', 'no']}
+        toggleBy="yes"
+        toggledRadios={['yes', 'no']}
+        toggledName="question_2"
+      />
+    );
+    expect(input.find('InputField').length).toEqual(2);
+  });
 });
